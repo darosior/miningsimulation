bb4aaf94756a02c4999dda3c5393981c53bfa9e8
Antoine Poinsot <darosior@protonmail.com>
2025-11-03 12:50:23 -0500

    A Python script to plot stale rate in function of propagation times

--- plot_stale_rate/sim.py
+++ plot_stale_rate/sim.py
@@ -1,105 +1,109 @@
 import math
+import matplotlib.pyplot as plt
 
+# Rate of arrival of blocks in seconds for the exponential distribution
 LAMBDA = 1 / 600
 
-# 
-MAJOR_POOLS = [("FOUNDRY", 0.29), ("ANTPOOL", 0.3), ("VIABTC", 0.12), ("F2POOL", 0.11), ("SPIDER", 0.08), ("MARA", 0.05), ("SECPOOL", 0.03)]  # 97%
+# Hashrate distribution. Covers 97%.
+MAJOR_POOLS = {"ANTPOOL": 0.3, "FOUNDRY": 0.29, "VIABTC": 0.12, "F2POOL": 0.11, "SPIDER": 0.08, "MARA": 0.05, "SECPOOL": 0.03}
 
-POOLS = MAJOR_POOLS + [("SMALL", 0.012), ("VERYSMALL", 0.006), ("TINY", 0.002)]
+# The remaining 3% are some invented smaller pools.
+SMALL_POOLS = {"SMALL": 0.012, "VERYSMALL": 0.006, "TINY": 0.002}
 
-# The propagation times (in seconds) to get the stale rate of each pool for.
-prop_times = list(range(30 + 1))
-
-# Mapping from pool name to a list of stale rates at various propagation time values.
-stale_rates = {}
+# The entire distribution of network hashrate.
+POOLS = {}
+POOLS.update(MAJOR_POOLS)
+POOLS.update(SMALL_POOLS)
 
 def exp_dist_cdf(lambda_param, duration):
     """Exponential distribution CDF for given duration."""
     return 1 - math.exp(-lambda_param * duration)
 
-def p_finds_before(prop_sec, h):
+def p_finds_within(prop_sec, h):
     """Probability that a miner with share {h} of network hashrate finds a block
     within {prop_sec} seconds."""
     this_lambda = LAMBDA * h
     return exp_dist_cdf(this_lambda, prop_sec)
 
-for (name, h) in POOLS:
-    stale_rates[name] = []
-    for prop_sec in prop_times:
-        other_pools = [p for p in POOLS if p != (name, h)]
+def p_stale_before(prop_sec, h):
+    """Probability that a block found by a miner with share {h} of network hashrate
+    goes stale due to another miner having found a block under {prop_sec} before."""
+    p_race = p_finds_within(prop_sec, 1 - h)
+    p_finds_next = 1 - h
+    return p_race * p_finds_next
 
-        # Probability that another miner finds a block before they receive ours,
-        # and then mines another on top before we do.
-        p_after = 0
-        for (_, o_h) in other_pools:
-            p_after += p_finds_before(prop_sec, o_h) * o_h
+def p_stale_after(prop_sec, net_hs):
+    """Probability that any miner from the list {net_hs} of network hashrates finds
+    a block within {prop_sec} and then finds another block on top."""
+    return sum(p_finds_within(prop_sec, h) * h for h in net_hs)
 
-        # Probability they found a block within the T seconds before we found ours,
-        # and that another block is found on top (by the entire rest of the network
-        # since their block would have reached them before ours did -- although when
-        # their block does reach all the other miners we need to take into account the
-        # probability that they had found one themselves before).
-        p_before = 0
-        for (_, o_h) in other_pools:
-            # Lambda parameter for this specific miner
-            this_lambda = LAMBDA * o_h
-            # This miner happens to have found a block in the T seconds that preceded
-            # our announcement.
-            p_found = (1 - math.exp(-this_lambda * prop_sec))
-            # The probability that all the other miners start mining on this block is
-            # that they didn't find one in the T seconds that preceded this other miner's
-            # block announcement.
-            h_rest = 1 - h - o_h
-            lambda_rest = LAMBDA * h_rest
-            p_other_mine = (1 - math.exp(-lambda_rest * prop_sec))
-            # The probability that this miner found a block before us AND another block
-            # is found on top of that.
-            p_before += p_found * (o_h + h_rest * p_other_mine)
+def get_stale_rates(prop_times):
+    """Get a mapping from pool name to a list of stale rates at various propagation times."""
+    stale_rates = {}
 
-        # An alternative to compute p_before that more accurately compute the hashrate
-        # mining on the competing block
-        p_bef = 0
-        for (o_name, o_h) in other_pools:
-            # Lambda parameter for this specific miner
-            this_lambda = LAMBDA * o_h
-            # This miner happens to have found a block in the T seconds that preceded
-            # our announcement.
-            p_found = (1 - math.exp(-this_lambda * prop_sec))
-            # The probability that all the other miners start mining on this block is
-            # that they didn't find one in the T seconds that preceded this other miner's
-            # block announcement.
-            h_rest = 0
-            rest_pools = [p for p in other_pools if p != (o_name, o_h)]
-            h_rest = sum((1 - p_finds_before(prop_sec, r_h)) * r_h for (_, r_h) in rest_pools)
-            #print(h, o_h, h_rest)
-            # The probability that this miner found a block before us AND another block
-            # is found on top of that.
-            p_bef += p_found * (o_h + h_rest)
+    for (name, hashrate) in POOLS.items():
+        stale_rates[name] = []
+        for prop_sec in prop_times:
+            # The stale rate for this miner depends on the probability of its block going
+            # stale due to another miner finding a block before it did, but it hadn't heard
+            # about it yet, plus the probability of it going stale because any other miner
+            # found a competing block before it reached them.
+            p_before = p_stale_before(prop_sec, hashrate)
+            network_hashrates = [h for (o_name, h) in POOLS.items() if (o_name, h) != (name, hashrate)]
+            p_after = p_stale_after(prop_sec, network_hashrates)
+            stale_rates[name].append(p_before + p_after)
 
-        p_other_race = sum(p_finds_before(prop_sec, o_h) for (_, o_h) in other_pools)
-        p_before_new = (1 - h) * p_other_race
+    return stale_rates
 
-        p_single_race = sum(p_finds_before(prop_sec * 2, o_h) * o_h for (_, o_h) in other_pools)
+def get_net_benefits(prop_times):
+    """Get a mapping from pool name to a list of advantages (in additional percentages of
+    blocks found) at various propagation times."""
+    stale_rates = get_stale_rates(prop_times)
 
-        print(prop_sec, h, p_single_race, p_after, p_before, p_before_new, p_bef)
+    benefits = {}
+    for i in range(len(prop_times)):
+        # Because the difficulty readjusts, we are interested in the proportion of blocks
+        # found by this miner *among* all the blocks actually accepted by the network (ie,
+        # discounting the stale blocks).
+        total_stale = sum(POOLS[name] * r[i] for name, r in stale_rates.items())
+        total_found = 1 - total_stale
+        for name in stale_rates:
+            if name not in benefits:
+                benefits[name] = []
+            actual_share = POOLS[name] * (1 - stale_rates[name][i]) / total_found
+            benefits[name].append(actual_share - POOLS[name])
 
-        # FIXME: decide
-        p_before = p_before_new
-        p_before = p_bef
+    return benefits
 
-        stale_rates[name].append((p_before + p_after) * 100)  # %
+def plot_stale_rates(prop_times):
+    stale_rates = get_stale_rates(prop_times)
 
-        # TODO: should take into account diff adjustment
+    fig, ax = plt.subplots()
+    for name in stale_rates:
+        rates_perc = [rate * 100 for rate in stale_rates[name]]
+        ax.plot(prop_times, rates_perc, label=name)
+    ax.set_xlabel("Propagation time (seconds)")
+    ax.set_ylabel("Stale rate (%)")
+    ax.legend(reverse=True)
 
+    plt.show()
 
+def plot_benefits(prop_times):
+    benefits = get_net_benefits(prop_times)
 
-import matplotlib.pyplot as plt
+    fig, ax = plt.subplots()
+    for name in benefits:
+        rates_perc = [rate * 100 for rate in benefits[name]]
+        ax.plot(prop_times, rates_perc, label=name)
+    ax.set_xlabel("Propagation time (seconds)")
+    ax.set_ylabel("Net benefit (% change in blocks found)")
+    ax.legend()
 
-fig, ax = plt.subplots()
-for name in stale_rates:
-    ax.plot(prop_times, stale_rates[name], label=name)
-ax.set_xlabel("Propagation time (seconds)")
-ax.set_ylabel("Stale rate (%)")
-ax.legend()
-plt.show()
+    plt.show()
 
+if __name__ == "__main__":
+    # The propagation times (in seconds) to get the stale rate of each pool for.
+    prop_times = list(range(20 + 1))
+
+    plot_stale_rates(prop_times)
+    plot_benefits(prop_times)
